2828f591401d57153e2d85a317c630120e1b6bba
Tedi <theodorechoi098@gmail.com>
2022-12-18 18:21:33 -0500

    Added gunshot sounds to all guns atm

--- Project Pong/Assets/Scripts/MuzzleFlash.cs
+++ Project Pong/Assets/Scripts/MuzzleFlash.cs
@@ -9,16 +9,22 @@ public class MuzzleFlash : MonoBehaviour
     public float mzMaxTimer = 1f;
     public float timer = 0f;
     public bool isFlashing;
+    [Header("SFX")]
+    public AudioManager audioManager;
+    public WeaponsSwitchSystem switchSystem;
+    private int sfxNumber;
+    private string currentGunName;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentGunName = switchSystem.activeGun.gunName;
     }
 
     // Update is called once per frame
     void Update()
     {
+        AudioManager();
         timer += Time.deltaTime;
         if(timer > mzMaxTimer)
         {
@@ -31,6 +37,26 @@ public class MuzzleFlash : MonoBehaviour
     {
         isFlashing = true;
         muzzleFlash.SetActive(true);
+        audioManager.PlayerSFX(sfxNumber);
         timer = 0f;
     }
+
+    void AudioManager()
+    {
+        switch(currentGunName)
+        {
+            case "SniperRifle":
+                sfxNumber = 0;
+                break;
+            case "DrumGun":
+                sfxNumber = 1;
+                break;
+            case "BipodGun":
+                sfxNumber = 2;
+                break;
+            default:
+                break;
+        }
+    }
+
 }
